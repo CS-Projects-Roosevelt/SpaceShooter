432e264e7242f2ad73442829fe5e0a9d94989ba8
JamesMertz <jamesmertz@me.com>
2020-02-27 15:21:15 -0800

    First object and movement

---
+++ Assets/ShipMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+	private float speed = 1.5f;
+
+	public float Speed { get => speed; set => speed = value; }
+
+	// Update is called once per frame
+	void Update()
+    {
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.position += Vector3.up * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.position += Vector3.down * speed * Time.deltaTime;
+            }
+        }
+    }
+}
