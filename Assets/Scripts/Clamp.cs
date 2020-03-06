aab8ecde07e6d7deee6a524b6ca5bbacd9f9d352
JamesMertz <jamesmertz@me.com>
2020-03-06 15:25:53 -0800

    New movement and camera scripts

---
+++ Assets/Scripts/Clamp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clamp : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = player.position + offset;
+        //transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+    }
+}
--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -9,35 +9,63 @@ public class ShipMovement : MonoBehaviour
     {
       
     }
+    private float damp = 0.5f;
 	private float speed = 30.0f;
-
+    public GameObject camera;
 	public float Speed { get => speed; set => speed = value; }
 
 	// Update is called once per frame
 	void Update()
     {
-        {
-            if (Input.GetKey(KeyCode.LeftArrow))
+        camera.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+
+        if ((Input.GetKey(KeyCode.DownArrow)) && (Input.GetKey(KeyCode.LeftArrow)))
             {
-               
-                transform.rotation = Quaternion.Euler(new Vector3(0,0,90));
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 125));
+                transform.position += Vector3.down * speed * Time.deltaTime * damp; //Vector3(-1,-1,0)
+                transform.position += Vector3.left * speed * Time.deltaTime * damp;
+            }
+            else if ((Input.GetKey(KeyCode.DownArrow)) && (Input.GetKey(KeyCode.RightArrow)))
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 225));
+                transform.position += Vector3.down * speed * Time.deltaTime * damp;
+                transform.position += Vector3.right * speed * Time.deltaTime * damp;
+            }
+            else if ((Input.GetKey(KeyCode.UpArrow)) && (Input.GetKey(KeyCode.LeftArrow)))
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 45));
+                transform.position += Vector3.up * speed * Time.deltaTime * damp; //Vector3(-1,-1,0)
+                transform.position += Vector3.left * speed * Time.deltaTime * damp;
+            }
+            else if ((Input.GetKey(KeyCode.UpArrow)) && (Input.GetKey(KeyCode.RightArrow)))
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 315));
+                transform.position += Vector3.up * speed * Time.deltaTime * damp;
+                transform.position += Vector3.right * speed * Time.deltaTime * damp;
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
                 transform.position += Vector3.left * speed * Time.deltaTime;
             }
-            if (Input.GetKey(KeyCode.RightArrow))
+            else if (Input.GetKey(KeyCode.RightArrow))
             {
                 transform.rotation = Quaternion.Euler(new Vector3(0, 0, 270));
                 transform.position += Vector3.right * speed * Time.deltaTime;
             }
-            if (Input.GetKey(KeyCode.UpArrow))
+            else if (Input.GetKey(KeyCode.UpArrow))
             {
                 transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
                 transform.position += Vector3.up * speed * Time.deltaTime;
             }
-            if (Input.GetKey(KeyCode.DownArrow))
+            else if (Input.GetKey(KeyCode.DownArrow))
             {
                 transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
                 transform.position += Vector3.down * speed * Time.deltaTime;
             }
-        }
+           
+            
+        camera.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
     }
 }
