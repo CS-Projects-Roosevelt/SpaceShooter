9f87a8a5adb632f58a4015ca0493a74e63b4bfda
JamesMertz <jamesmertz@me.com>
2020-05-05 10:51:23 -0700

    a few tweaks

---
+++ Assets/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine("AutoDelete");
+    }
+
+    //moves backwards at a constant speed
+    private void Update()
+    {
+        rb.velocity = Vector3.up * speed;
+        Debug.Log(rb.velocity);
+    }
+
+    IEnumerator AutoDelete()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/BulletPrototype1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+public class BulletPrototype1 : MonoBehaviour
+{
+    public float maxSpeed = 25f;
+    public Rigidbody2D bullet;
+    public float[] xPositions;
+    public float[] yPositions;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            /*
+             * Rigidbody2D bulletInstance = Instantiate(bullet, transform.position, Quaternion.Euler(new Vector3(0, 0, 1))) as Rigidbody2D;
+            bulletInstance.velocity = transform.forward * maxSpeed;
+            */
+            int randX = Random.Range(0, xPositions.Length);
+            int randY = Random.Range(0, yPositions.Length);
+            Vector3 randPosition = new Vector3(xPositions[randX], yPositions[randY], transform.position.z);
+
+            Instantiate(bullet, randPosition, Quaternion.identity);
+        }
+    }
+}
